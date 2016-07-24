e533d6b9331405b946cc78580626f8dde4b75a05
Kamil Sikorski <Kamil Sikorski>
2016-07-24 05:15:03 +0200

    config desktop launcher

--- core/src/com/mygdx/game/ClickerGdxGame.java
+++ core/src/com/mygdx/game/ClickerGdxGame.java
@@ -1,33 +1,33 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.mygdx.game.screens.SplashScreen;
 
-public class ClickerGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class ClickerGdxGame extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    public static final String GAME_NAME = "Clicker";
+
+    public static final int WIDTH = 480;
+    public static final int HEIGHT = 700;
+
+    private boolean paused;
+
+
+
+    @Override
+    public void create() {
+        this.setScreen(new SplashScreen(this));
+
+    }
+
+    /*
+    getters and setters
+    */
+    public boolean isPaused() {
+        return paused;
+    }
+
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.ClickerGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ClickerGdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.title = ClickerGdxGame.GAME_NAME;
+        config.width = ClickerGdxGame.WIDTH;
+        config.height = ClickerGdxGame.HEIGHT;
+        config.resizable = false;
+
+        new LwjglApplication(new ClickerGdxGame(), config);
+    }
 }
