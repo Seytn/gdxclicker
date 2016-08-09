b76e6ff4cdb852affa4e714f137f0b4c16327ff3
Kamil Sikorski <camillo18@gmail.com>
2016-08-10 00:59:22 +0200

    Add click button and click listener

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,5 +1,8 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.ClickerGdxGame;
 import com.mygdx.game.entities.Player;
 
@@ -9,11 +12,33 @@ import com.mygdx.game.entities.Player;
 public class GameplayScreen extends AbstractScreen{
 
     private Player player;
+    private Button playerButton;
 
     public GameplayScreen(ClickerGdxGame game) {
         super(game);
 
         initPlayer();
+        initPlayerButton();
+    }
+
+    private void initPlayerButton() {
+        playerButton = new Button(new Button.ButtonStyle());
+        playerButton.setWidth(460);
+        playerButton.setHeight(360);
+        playerButton.setX(10);
+        playerButton.setY(170);
+        playerButton.setDebug(true);
+
+        stage.addActor(playerButton);
+
+        playerButton.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                
+
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
     }
 
     @Override
