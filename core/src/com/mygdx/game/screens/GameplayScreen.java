40c638f90bd6656dae68e3d4e1040e9fb34d1234
SATZAVKA\Kamil <camillo18@gmail.com>
2016-08-09 16:27:47 +0200

    added simply GameplayScreen

---
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.screens;
+
+import com.mygdx.game.ClickerGdxGame;
+
+/**
+ * Created by Kamil on 2016-08-09.
+ */
+public class GameplayScreen extends AbstractScreen{
+
+    public GameplayScreen(ClickerGdxGame game) {
+        super(game);
+        
+        init();
+        
+    }
+
+    private void init() {
+    }
+}
