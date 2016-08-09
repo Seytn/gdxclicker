0f3f66d809e63a5c87cb82b24f2ed2478c600017
Kamil Sikorski <camillo18@gmail.com>
2016-08-09 17:01:03 +0200

    Display player

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,19 +1,42 @@
 package com.mygdx.game.screens;
 
 import com.mygdx.game.ClickerGdxGame;
+import com.mygdx.game.entities.Player;
 
 /**
  * Created by Kamil on 2016-08-09.
  */
 public class GameplayScreen extends AbstractScreen{
 
+    private Player player;
+
     public GameplayScreen(ClickerGdxGame game) {
         super(game);
         
         init();
-        
+        initPlayer();
+    }
+
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
     }
 
     private void init() {
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+
+    }
+
+    private void update() {
+        stage.act();
     }
 }
